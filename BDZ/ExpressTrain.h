fdaeef2e7a7c5a8274f4c7db8d30240502420cc9
Denislav Ivanov <dyivanov98@gmail.com>
2020-07-16 22:41:45 +0300

    Rename variables and method names

--- BDZ/ExpressTrain.h
+++ BDZ/ExpressTrain.h
@@ -1,18 +1,19 @@
-#ifndef EXPRESS_H
-#define EXPRESS_H
+#ifndef EXPRESSTRAIN_H
+#define EXPRESSTRAIN_H
 #include "QuickTrain.h"
 #include "DirectTrain.h"
 
-class Express: virtual public QuickTrain, virtual public DirectTrain{
+class ExpressTrain: virtual public QuickTrain, virtual public DirectTrain
+{
 public:
-    Express();
-    Express(int numberOfWagons, const int* capacityOfWagons, int speed, double fasterBy, int trainPriority);
+    ExpressTrain();
+    ExpressTrain(int numberOfWagons, const int* capacityOfWagons, int getTrainSpeed, double fasterBy, int trainPriority);
     virtual Train* clone() const;
 
-    friend std::ostream& operator<<(std::ostream& os, const Express& g);
-    friend std::istream& operator>>(std::istream& is, Express& q);
+    friend std::ostream& operator<<(std::ostream& os, const ExpressTrain& g);
+    friend std::istream& operator>>(std::istream& is, ExpressTrain& q);
 
-    void Print(std::ostream& os) const;
-    void Read(std::istream& is);
+    void printTrainInformation(std::ostream& os) const;
+    void readTrainInformation(std::istream& is);
 };
-#endif // EXPRESS_H
+#endif // EXPRESSTRAIN_H
