057cff0bb24e6f2db07a67467f532224e056e2a7
Denislav Ivanov <dyivanov98@gmail.com>
2020-07-16 21:48:58 +0300

    Rename variables and method names

--- BDZ/Train.h
+++ BDZ/Train.h
@@ -2,24 +2,24 @@
 #define TRAIN_H
 #include<iostream>
 #include "Station.h"
+
 class Station;
 class  Train{
 private:
     int numberOfWagons;
     int* capacityOfWagons;
-    int speed;
-    int currentStation;
-    int numberOfStations;
-    Station* stations;
+    int trainSpeed;
+    int currentTrainStation;
+    int numberOfTrainStation;
+    Station* trainStation;
 
     void Copy(const Train& t);
     void Erase();
     void Resize();
 
-
 public:
     Train();
-    Train(int numberOfWagons, const int* capacityOfWagons, int speed);
+    Train(int numberOfWagons, const int* capacityOfWagons, int trainSpeed);
     Train(const Train& t);
     Train& operator = (const Train& t);
     friend std::ostream& operator<<(std::ostream& os, const Train& t);
@@ -27,15 +27,15 @@ public:
     virtual Train* clone() const;
     virtual~Train();
 
-    void Print(std::ostream& os) const;
-    void Read(std::istream& is);
+    void printTrainInformation(std::ostream& os) const;
+    void readTrainInformation(std::istream& is);
 
-    void addStation(const Station& s);
-    void removeStation(const Station& s);
+    void addTrainStation(const Station& s);
+    void removeTrainStation(const Station& s);
 
     void setCapacityOfWagons(int numberOfWagons, const int* capacityOfWagons);
-    int getSpeed()const;
-    int getCurrentStation()const;
-    const Station* getStations()const;
+    int getTrainSpeed()const;
+    int getCurrentTrainStation()const;
+    const Station* getListOfTrainStations()const;
 };
 #endif // TRAIN_H
