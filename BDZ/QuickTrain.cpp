6f47623ac66c947625530b5363339b213cc2388e
Denislav Ivanov <dyivanov98@gmail.com>
2020-07-17 20:11:27 +0300

    Update QuickTrain.cpp
    Change brackets convention
    Add new lines and spaces for readability
    Rename function names
    Rename variables names
    Change function order

--- BDZ/QuickTrain.cpp
+++ BDZ/QuickTrain.cpp
@@ -1,41 +1,57 @@
 #include "QuickTrain.h"
 #include "Train.h"
 #include<iostream>
-using namespace std;
-void QuickTrain::setFasterBy(double x){
-    if(x >= 1 && x <=3){
-        fasterBy = getSpeed() * x;
-    }
-    else
-    {
-        cout<<"1-3"<<endl;
-    }
-}
-double QuickTrain::getFasterBy()const{
-    return fasterBy;
-}
-QuickTrain::QuickTrain():Train(), fasterBy(0){}
-QuickTrain::QuickTrain(int numberOfWagons, const int* capacityOfWagons, int speed, double fasterBy):
-    Train(numberOfWagons, capacityOfWagons, speed * fasterBy), fasterBy(fasterBy){}
+#define Index_MIN 1
+#define Index_MAX 3
 
-void QuickTrain::Print(std::ostream &os) const{
-    Train::Print(os);
-    os<<"Faster by: "<<fasterBy<<endl;
+QuickTrain::QuickTrain():Train(), fasterBy(0){}
+
+QuickTrain::QuickTrain(int numberOfWagons, const int* capacityOfWagons, int trainSpeed, double fasterBy):
+                 Train(numberOfWagons, capacityOfWagons, trainSpeed * fasterBy), fasterBy(fasterBy){}
+
+std::ostream &operator<<(std::ostream &os, const QuickTrain &d) 
+{
+    d.printTrainInformation(os);
+    return os;
 }
-void QuickTrain::Read(std::istream &is){
-    Train::Read(is);
-    cout<<"Faster by: ";
+
+std::istream &operator>>(std::istream &is, QuickTrain &d) 
+{
+    d.readTrainInformation(is);
+    return is;
+}
+
+Train *QuickTrain::clone() const {
+    return new QuickTrain(*this);
+}
+
+void QuickTrain::printTrainInformation(std::ostream &os) const
+{
+    Train::printTrainInformation(os);
+    os<<"Faster by: "<<fasterBy<<std::endl;
+}
+
+void QuickTrain::readTrainInformation(std::istream &is)
+{
+    Train::readTrainInformation(is);
+    std::cout<<"Faster by: ";
     is>>fasterBy;
     is.ignore();
 }
-std::ostream &operator<<(std::ostream &os, const QuickTrain &d) {
-    d.Print(os);
-    return os;
+
+void QuickTrain::setFasterBy(double index)
+{
+    if(Index_MIN <= index && index <= Index_MAX)
+    {
+        fasterBy = getTrainSpeed() * index;
+    }
+    else
+    {
+        std::cout<<"Please enter sped index in [1,3]"<<std::endl;
+    }
 }
-std::istream &operator>>(std::istream &is, QuickTrain &d) {
-    d.Read(is);
-    return is;
-}
-Train *QuickTrain::clone() const {
-    return new QuickTrain(*this);
+
+double QuickTrain::getFasterBy()const
+{
+    return fasterBy;
 }
