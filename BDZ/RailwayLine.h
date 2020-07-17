ab7d891429434d5bb7de70b0f21fb5af9b90d5b6
Denislav Ivanov <dyivanov98@gmail.com>
2020-07-17 20:15:48 +0300

    Rename variables and method names

--- BDZ/RailwayLine.h
+++ BDZ/RailwayLine.h
@@ -1,34 +1,36 @@
 #ifndef RAILWAYLINE_H
 #define RAILWAYLINE_H
-
 #include "Station.h"
-class Railwayline{
+
+class Railwayline
+{
 private:
-    char* currentStop;
-    char* lastStop;
-    double departTime;
-    double arriveTime;
+    char* currentTrainStop;
+    char* lastTrainStop;
+    double trainDepartTime;
+    double trainArriveTime;
 
     Train* train;
 
     void Copy(const Railwayline& r);
     void Erase();
+    
 public:
     Railwayline();
-    Railwayline(char* currentStop, char* lastStop, double departTime, double arriveTime);
+    Railwayline(char* currentTrainStop, char* lastTrainStop, double trainDepartTime, double trainArriveTime);
     Railwayline(const Railwayline& r);
     Railwayline& operator=(const Railwayline& r);
     bool operator ==( Railwayline& r);
     ~Railwayline();
 
-
     void setTrainPointer(const Train& t);
+    
     Train* getTrainPointer()const;
-    double getDepartTime()const;
-    double getArriveTime()const;
-    const char* getCurrentStop()const;
-    const char* getLastStop()const;
+    double getTrainDepartTime()const;
+    double getTrainArriveTime()const;
+    const char* getTrainCurrentStop()const;
+    const char* getTrainLastStop()const;
 
-    void printLine() const;
+    void printRailwayLineInformation() const;
 };
 #endif // RAILWAYLINE_H
