29ee47a7166d14bfb9773d3de0b70dcd4774aaeb
Denislav Ivanov <dyivanov98@gmail.com>
2020-07-17 20:03:34 +0300

    Rename method names

--- BDZ/QuickTrain.h
+++ BDZ/QuickTrain.h
@@ -2,20 +2,23 @@
 #define QUICKTRAIN_H
 #include "Train.h"
 #include "Station.h"
-class QuickTrain: public virtual Train{
+
+class QuickTrain: public virtual Train
+{
 protected:
     double fasterBy;
+    
 public:
     QuickTrain();
     QuickTrain(int numberOfWagons, const int* capacityOfWagons, int speed, double fasterBy);
+    friend std::ostream& operator<<(std::ostream& os, const QuickTrain& q);
+    friend std::istream& operator>>(std::istream& is, QuickTrain& q);
     virtual Train* clone() const;
 
     void setFasterBy(double x);
     double getFasterBy()const;
-    void Print(std::ostream& os) const;
-    void Read(std::istream& is);
-    friend std::ostream& operator<<(std::ostream& os, const QuickTrain& q);
-    friend std::istream& operator>>(std::istream& is, QuickTrain& q);
+    void printTrainInformation(std::ostream& os) const;
+    void readTrainInformation(std::istream& is);
 };
 #endif // QUICKTRAIN_H
 
