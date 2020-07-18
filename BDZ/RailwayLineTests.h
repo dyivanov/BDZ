7eb386c664b7def3c8aa20728c783d882be45ac9
Denislav Ivanov <dyivanov98@gmail.com>
2020-07-18 13:08:56 +0300

    Create unit test file for class Railwayline

---
+++ BDZ/RailwayLineTests.h
@@ -0,0 +1,13 @@
+#ifndef RAILWAYLINETESTS_H
+#define RAILWAYLINETESTS_H
+
+#include "pch.h"
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+#include "RailwayLine.h"
+
+TEST_CASE("Create empty test case")
+{
+    //TODO
+}
+#endif // RAILWAYLINETESTS_H
