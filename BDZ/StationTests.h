063ec2d9d7e55bbab69fbfc09493a7abb9ee2beb
Denislav Ivanov <dyivanov98@gmail.com>
2020-07-18 12:55:47 +0300

    Add new unit tests for class Station

--- BDZ/StationTests.h
+++ BDZ/StationTests.h
@@ -6,8 +6,40 @@
 #include "Train.h"
 #include "Station.h"
 
-TEST_CASE("Empty test case")
+TEST_CASE("Create empty station")
 {
-	//TODO
+	Station station();
+	CHECK(station.getTrainPriority() == '1');
+	CHECK(station.getNameOfStation() == 'nullptr');
+	CHECK(station.getX() == '0');
+	CHECK(station.getY() == '0');
+}
+
+TEST_CASE("Create station Sofia")
+{
+	Station station("Sofia", 10 , 12, 15);
+	CHECK(station.getTrainPriority() == '10');
+	CHECK(station.getNameOfStation() == 'Sofia');
+	CHECK(station.getX() == '12');
+	CHECK(station.getY() == '15');
+}
+
+TEST_CASE("Test case for operators =")
+{
+	Station Sofia("Sofia", 10 , 12, 15);
+	Station Burgas("Burgas", 7 , 10, 19);
+	Sofia = Burgas;
+	
+	CHECK(Sofia.getTrainPriority() == '7');
+	CHECK(Sofia.getNameOfStation() == 'Burgas');
+	CHECK(Sofia.getX() == '10');
+	CHECK(Sofia.getY() == '19');
+}
+
+TEST_CASE("Test case for operators ==")
+{
+	Station Sofia("Sofia", 10 , 12, 15);
+	Station Burgas("Burgas", 7 , 10, 19);
+	CHECK(!(Sofia == Burgas));
 }
 #endif // STATIONTESTS_H
