cd60c06f92caa4be9ff59f31a596ae0ad4590daf
Denislav Ivanov <dyivanov98@gmail.com>
2020-07-18 12:47:55 +0300

    Create unit test file for class Station

---
+++ BDZ/StationTests.h
@@ -0,0 +1,13 @@
+#ifndef STATIONTESTS_H
+#define STATIONTESTS_H
+#include "pch.h"
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+#include "Train.h"
+#include "Station.h"
+
+TEST_CASE("Empty test case")
+{
+	//TODO
+}
+#endif // STATIONTESTS_H
